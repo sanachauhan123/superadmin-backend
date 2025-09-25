0ea1a468666284938f00d0ebbc1aecebe0c08458
sanachauhan123 <sanac922@gmail.com>
2025-09-25 15:24:45 +0530

    Update tables.js

--- routes/tables.js
+++ routes/tables.js
@@ -6,43 +6,25 @@ const { verifyRestaurant } = require('../middleware/auth');
 // GET all tables
 router.get('/',verifyRestaurant, async (req, res) => {
   try {
-    //console.log('req.query.restaurantId:', req.query.restaurantId);
-   // const tables = await Table.find({ restaurantId: req.restaurantId });
+    //const tables = await Table.find({ restaurantId: req.restaurantId });
     const tables = await Table.find({});
-    res.json({ success: true, data: tables });
+    res.json({ data: tables });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
-
-// GET a specific table by ID
-router.get('/:id', verifyRestaurant, async (req, res) => {
-  try {
-    const table = await Table.find({
-      _id: req.params.id,
-      restaurantId: req.restaurantId,
-    });
-    console.log(table)
-
-    if (!table) return res.status(404).json({ error: 'Table not found' });
-
-    res.json({ success: true, data: table });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
 
 // CREATE
 router.post('/', verifyRestaurant, async (req, res) => {
   console.log('req.restaurantId =>', req.restaurantId); // ✅ Add this
 
   try {
-    const { number, capacity } = req.body;
+    const { number, capacity, status } = req.body;
 
     const newTable = new Table({
       number,
       capacity,
+      status,
       restaurantId: req.restaurantId, // This must not be undefined
     });
 
@@ -56,14 +38,26 @@ router.post('/', verifyRestaurant, async (req, res) => {
 // UPDATE
 router.put('/:id', verifyRestaurant, async (req, res) => {
   try {
-    const { number, capacity } = req.body;
+    const { number, capacity, status } = req.body;
     const updated = await Table.findOneAndUpdate(
       { _id: req.params.id, restaurantId: req.restaurantId },
-      { number, capacity },
+      { number, capacity, status },
       { new: true }
     );
     if (!updated) return res.status(404).json({ error: 'Table not found' });
     res.json({ message: 'Table updated successfully', data: updated });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.put("/reset", verifyRestaurant, async (req, res) => {
+  try {
+    await Table.updateMany(
+      { restaurantId: req.restaurantId },
+      { $set: { status: "Available" } }
+    );
+    res.json({ success: true });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
