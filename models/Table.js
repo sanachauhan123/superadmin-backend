d869d597f6e48ddcbbdc06567e1731ef147239db
sanachauhan123 <sanac922@gmail.com>
2025-09-25 15:25:10 +0530

    Update Table.js

--- models/Table.js
+++ models/Table.js
@@ -4,6 +4,11 @@ const tableSchema = new mongoose.Schema({
   number: { type: String, required: true },
   capacity: { type: Number, required: true },
   restaurantId: { type: String, required: true },
+  status: {
+    type: String,
+    enum: ['pending', 'in-kitchen', 'served', 'completed', 'cancelled', 'Available'],
+    default: 'Available'
+  },
   createdAt: { type: Date, default: Date.now }
 });
 
