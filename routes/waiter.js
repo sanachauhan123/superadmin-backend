ac7074d2b93d93a3dc23bb4c2665fe38dcd30567
Sana Chauhan <sanac922@gmail.com>
2025-10-07 15:51:19 +0530

    Update waiter.js

--- routes/waiter.js
+++ routes/waiter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Waiter = require("../models/Waiter");
+const Restaurant = require("../models/Restaurant");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
