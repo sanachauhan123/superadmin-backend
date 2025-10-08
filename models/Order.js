c53e541945f5907987ddef0ef4afe985f4a55df9
Sana Chauhan <sanac922@gmail.com>
2025-10-08 15:22:00 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -8,7 +8,13 @@ const orderSchema = new mongoose.Schema({
       menuId: { type: String, required: true },
       name: { type: String, required: true },
       quantity: { type: Number, required: true },
-      price: { type: Number, required: true }
+      price: { type: Number, required: true },
+      status: {
+        type: String,
+        enum: ['pending', 'served', 'cancelled'],
+        default: 'pending'
+      }
+
     }
   ],
 
