c3caed5e7ef26e360c2486f87f37dcda628fca98
Sana Chauhan <sanac922@gmail.com>
2025-10-08 15:46:41 +0530

    Update Waiter.js

--- models/Waiter.js
+++ models/Waiter.js
@@ -4,7 +4,7 @@ const waiterSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true },
   password: { type: String, required: true },
-  restaurantId: { type: String, unique: true },
+  restaurantId: { type: String, required: true },
   restaurant: { type: String, required: true }, 
   image: { type: String, default: "" }, 
   createdAt: { type: Date, default: Date.now }
